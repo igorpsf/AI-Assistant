d466059b4ebd0f0ac3ec71b38bedad78184ed226
Igor Postnikov <igor.postnikov8@gmail.com>
2020-05-01 16:57:22 -0700

    added few examples

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+## Install brew
+1) `/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"`
+
 ## Install pip
 1) `curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py`
 2) `python get-pip.py`
--- app.py
+++ app.py
@@ -8,3 +8,40 @@ def talk(words):
     os.system("say " + words)
 
 talk("Hi, ask me something")
+
+def command():
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Say something")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source, duration=1)
+        audio = r.listen(source)
+
+    try:
+        task = r.recognize_google(audio, language="en-US").lower()
+        talk("You said: " + task)
+        #print("You said: " + task)
+    except sr.UnknownValueError:
+        talk("I didn't understand you")
+        task = command()
+
+    return task
+
+def makeSomething(task):
+    if "how are you" in task:
+        talk("I am good, thank you igor")
+
+    if "who is making coffee" in task:
+        talk("It is Dmitri who likes to drink coffee")
+
+    if "open robinhood" in task:
+        talk("Opening")
+        url = "https://www.robinhood.com"
+        webbrowser.open(url)
+    elif "stop" in task:
+        talk("Sure")
+        sys.exit()
+
+while True:
+    makeSomething(command())
